ad878e1e79c311cc2a3aefa36cbef3d9617b25cb
Chuc Nguyen <nhiennguyen1605@gmail.com>
2022-05-26 09:41:39 +0700

    Fisrt commit

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+export const images = {
+  background: require('./images/background.png'),
+  buy: require('./images/buy.png'),
+  start: require('./images/start.png'),
+  close: require('./images/close.png'),
+  ok: require('./images/ok.png'),
+  shopvar: require('./images/shopvar.png'),
+  v1: require('./images/v1.png'),
+  v2: require('./images/v2.png'),
+  v3: require('./images/v3.png'),
+  v4: require('./images/v4.png'),
+  v5: require('./images/v5.png'),
+  v6: require('./images/v6.png'),
+  v7: require('./images/v7.png'),
+  v8: require('./images/v8.png'),
+  yourscore: require('./images/yourscore.png'),
+}
---
+++ src/conf.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    sku: 'speculate_100',
+    value: 100,
+  },
+  {
+    sku: 'speculate_200',
+    value: 200,
+  },
+  {
+    sku: 'speculate_300',
+    value: 300,
+  },
+  {
+    sku: 'speculate_400',
+    value: 400,
+  },
+];
